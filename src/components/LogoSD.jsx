7e598093cdb50eea697bf779c56a56cc9112eb49
Hadyan Abdul Aziz <hadyanabdulaziz@gmail.com>
2025-07-02 10:55:42 +0700

    update view list guru

---
+++ src/components/LogoSD.jsx
@@ -0,0 +1,13 @@
+export default function LogoSD({titlePage}){
+   return (
+      <div className='flex content-center items-center my-auto gap-2'>
+         { titlePage ? '' 
+         : <img src="/assets/image/logo-kawan-belajar-bged.png" alt="Logo Kawan Belajar" className='w-auto h-8' />}
+         <h1 className="text-xl font-medium font-jakarta">
+            <span className='text-whiteprime mr-1'>{titlePage}</span>
+            <span className='text-primaryoren font-bold'>SDN</span>
+            <span className='text-whiteprime ml-1'>Ragunan 14 Pagi</span>
+         </h1>
+      </div>
+   )
+}
--- src/pages/Profile/ListGuru.jsx
+++ src/pages/Profile/ListGuru.jsx
@@ -1,6 +1,7 @@
 import { Card, CardMedia } from "@mui/material";
 import { useEffect, useState } from "react";
 import ImageViewer from "../../components/ImageViewer";
+import LogoSD from "../../components/LogoSD";
 
 export default function ListGuru() {
   const [ListGuru, setListGuru] = useState([]);
@@ -12,27 +13,30 @@ export default function ListGuru() {
       .catch(err => console.error("Gagal mengambil data:", err));
    }, []);
    return (
-      <>
-      <div className="grid grid-cols-4 gap-y-8 my-8">
-         {
-            ListGuru.map((item, index) => (
-               <Card
-                  className="mx-auto"
-                  sx={{height: "auto", width: 250, padding: 2, 
-                     backgroundImage: "url(/assets/image/pattern.png)", 
-                     backgroundColor: "#098fd1",
-                     textAlign: "center"
-                  }}
-               >
-                  <div className="flex justify-center">
-                     <ImageViewer imageUrl={item.url_image} fullImageUrl={item.url_image} />
-                  </div>
-                  <h4 className={`font-semibold text-lg mt-4 text-secondary`}>{item.nama}</h4>
-                  <p className={`text-sm text-whiteprime`}>{item.status}</p>
-               </Card>
-            ))
-         }
+      <div className="px-28 py-8">
+         <div className="flex justify-center bg-secondblue bg-[url(/assets/image/pattern.png)] p-4 rounded-lg w-full">
+            <LogoSD titlePage="Guru" />
+         </div>
+         <div className="grid grid-cols-4 gap-y-8 my-8">
+            {
+               ListGuru.map((item, index) => (
+                  <Card
+                     className="mx-auto"
+                     sx={{height: "auto", width: 250, padding: 2, 
+                        backgroundImage: "url(/assets/image/pattern.png)", 
+                        backgroundColor: "#098fd1",
+                        textAlign: "center"
+                     }}
+                  >
+                     <div className="flex justify-center">
+                        <ImageViewer imageUrl={item.url_image} fullImageUrl={item.url_image} />
+                     </div>
+                     <h4 className={`font-semibold text-lg mt-4 text-secondary`}>{item.nama}</h4>
+                     <p className={`text-sm text-whiteprime`}>{item.status}</p>
+                  </Card>
+               ))
+            }
+         </div>
       </div>
-      </>
    )
 }
