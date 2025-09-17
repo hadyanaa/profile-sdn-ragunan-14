3d1f88cd19227a4905d64e13e79ee4ac5a4fde91
Hadyan Abdul Aziz <hadyanabdulaziz@gmail.com>
2025-09-18 06:34:59 +0700

    hapus space putih di hp part 2

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,12 +1,12 @@
 export default function Footer() {
    return(
       <>
-      <footer className="text-gray-300">
+      <footer className="w-full text-gray-300">
          <div className="flex flex-row gap-16 scrolled-header px-28 py-4">
             <div>
                <div className='flex content-center items-center gap-2'>
                   <img src="/assets/image/logo-kawan-belajar-bged.png" alt="Logo Kawan Belajar" className='w-auto h-8' />
-                  <h1 className="text-xl font-medium font-jakarta text-whiteprime">
+                  <h1 className="text-xs lg:text-xl font-medium font-jakarta text-whiteprime">
                      <span className='text-primaryoren font-bold'>SDN</span>
                      <span className='ml-1'>Ragunan 14 Pagi</span>
                   </h1>
