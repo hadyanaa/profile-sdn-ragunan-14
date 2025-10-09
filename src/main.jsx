1aab25c54943f90f80c9efcf0b2938f9ed8675ec
Hadyan Abdul Aziz <hadyanabdulaziz@gmail.com>
2025-10-09 11:35:52 +0700

    create google analytics and delete analytics vercel

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import '@fontsource/plus-jakarta-sans'; // otomatis load regular (400)
+import ReactGA from "react-ga4";
+
+// Ganti dengan ID kamu dari Google Analytics
+ReactGA.initialize("G-1NB3PNYZPW");
+
+// Kirim event "pageview" saat aplikasi pertama kali dimuat
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
